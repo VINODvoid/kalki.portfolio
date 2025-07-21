2d72368485dc0f79f318929d74d68cc67a160f72
VINODvoid <iamlearner.v@gmail.com>
2025-07-21 17:07:40 +0530

    Refactor homepage layout and add reusable components for buttons and horizontal rules

--- app/layout.tsx
+++ app/layout.tsx
@@ -43,7 +43,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className="light">
       <body
         className={`${poppins.variable}antialiased`}
       >
