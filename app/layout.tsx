5b43f5657d7a727c27116106bfbdf1047f7ec974
VINODvoid <iamlearner.v@gmail.com>
2025-07-25 10:46:36 +0530

    refactor: Clean up layout.tsx and add favicon support with new icon.svg

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,41 +4,39 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import ClientTopProgressBar from "@/components/ClientTopProgressbar";
 
-
 const poppins = Poppins({
-   subsets:["latin"],
-   variable:"--font-poppins",
-   weight:["400","500","600","700"]
-})
-
-
+  subsets: ["latin"],
+  variable: "--font-poppins",
+  weight: ["400", "500", "600", "700"],
+});
 
 export const metadata: Metadata = {
   title: "Kalki | Portfolio",
-  description: "My name is Vinod Vardaram, I'm a Software Developer. The question isn't who am I, but when am I? ",
-  keywords:[
-   "kalki",
-   "kalki portfolio",
-   "vinod portfolio",
-   "vinod vardaram",
-   "vinod vard",
-   "vinod ram",
-   "kalki vinod",
-   "vinod kal",
-   "portfolio developer",
-   "vinod",
-   "portfolio vinod",
+  description:
+    "My name is Vinod Vardaram, I'm a Software Developer. The question isn't who am I, but when am I? ",
+  keywords: [
+    "kalki",
+    "kalki portfolio",
+    "vinod portfolio",
+    "vinod vardaram",
+    "vinod vard",
+    "vinod ram",
+    "kalki vinod",
+    "vinod kal",
+    "portfolio developer",
+    "vinod",
+    "portfolio vinod",
   ],
-  openGraph:{
-    type:"website",
-    title:"Kalki | Portfolio",
-    siteName:"Kalki | Portfolio",
-    description:"My name is Vinod Vardaram, I'm a Software Developer. The question isn't who am I, but when am I? ",
+  openGraph: {
+    type: "website",
+    title: "Kalki | Portfolio",
+    siteName: "Kalki | Portfolio",
+    description:
+      "My name is Vinod Vardaram, I'm a Software Developer. The question isn't who am I, but when am I? ",
   },
-  icons:{
-    icon:"/icon.png"
-  }
-
+  icons: {
+    icon: "/icon.svg",
+  },
 };
 
 export default function RootLayout({
@@ -48,11 +46,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable}antialiased`}
-      >
-        <ClientTopProgressBar/>
-        <Navbar/>
+      <head>
+        <link rel="icon" href="/favicon.png" type="image/png" sizes="32x32" />
+        <link rel="shortcut icon" href="/favicon.png" />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
+      <body className={`${poppins.variable}antialiased`}>
+        <ClientTopProgressBar />
+        <Navbar />
         {children}
       </body>
     </html>
